646692b0416a6713b242ecad97135c78f3b130ec
libill <770737185@qq.com>
2014-01-18 16:50:32 +0800

    add title

--- src/com/example/news/MainActivity.java
+++ src/com/example/news/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.Window;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
@@ -23,7 +24,6 @@ public class MainActivity extends TabActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.layout_bottom);
-		
         bottom_layout = (RelativeLayout) findViewById(R.id.layout_bottom);
         
         tabHost = getTabHost();
@@ -79,12 +79,5 @@ public class MainActivity extends TabActivity {
 			}
 		}
 	};
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
 
 }
